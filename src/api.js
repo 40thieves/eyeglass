c1ce20dc8f341ac073231b9d415f0839a80ae0ae
Alasdair Smith <ali@alasdairsmith.co.uk>
2015-08-20 22:55:54 +0100

    Working with server response

--- src/api.js
+++ src/api.js
@@ -1,7 +1,15 @@
 export default async (fakeError) => {
 	if (fakeError) throw new Error('Error thrown')
 
-	return fakeResponse()
+	//return fakeResponse()
+	return serverResponse()
+}
+
+const url = (endpoint) => `http://ib.api` + endpoint
+
+async function serverResponse(endpoint) {
+	let raw = await fetch(url(`/inspections/3`), { headers: { 'X-Api-Key': 'yqeY9Rfl3jcXOfbtytLj3MqT1' } })
+	return await raw.json()
 }
 
 // Faking out server response
