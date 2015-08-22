bfa0cd849b5e2cf10774de180b79c142acf01882
Alasdair Smith <ali@alasdairsmith.co.uk>
2015-08-22 21:25:22 +0100

    Process article data into component friendly format

--- src/actions.js
+++ src/actions.js
@@ -9,9 +9,11 @@ export function requestArticle() {
 
 export const RECEIVE_ARTICLE = 'RECEIVE_ARTICLE'
 export function receiveArticle(payload) {
+	let data = processHeader(payload);
+
 	return {
 		type: RECEIVE_ARTICLE,
-		payload: payload
+		payload: data
 	}
 }
 
@@ -34,5 +36,24 @@ export function fetchArticle(fail) {
 		.catch(error => {
 			dispatch(requestArticleError(error))
 		})
+	}
+}
+
+function processHeader(payload) {
+	let data = payload.nodes
+
+	let documentMeta = data.document
+	let publicationInfo = data.publication_info
+
+	// Pull author nodes out using author ids array
+	let authors = documentMeta.authors.map(function(id) {
+		return data[id]
+	})
+
+	return {
+		 publishDate: new Date(publicationInfo.published_on),
+		 title: documentMeta.title,
+		 authors: authors,
+		 doi: publicationInfo.doi
 	}
 }
