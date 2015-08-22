14a6acd9267616450d64c306ee9dbf7d60e28714
Alasdair Smith <ali@alasdairsmith.co.uk>
2015-08-22 20:55:51 +0100

    Load sample article

--- src/api.js
+++ src/api.js
@@ -1,15 +1,15 @@
 export default async (fakeError) => {
 	if (fakeError) throw new Error('Error thrown')
 
-	return fakeResponse()
-	//return serverResponse()
+	//return fakeResponse()
+	return serverResponse()
 }
 
 // Just a quick local server that I have set up (it's entirely unrelated to eyeglass, too lazy to set up a proper server)
 const url = (endpoint) => `http://ib.api` + endpoint
 
 async function serverResponse(endpoint) {
-	let raw = await fetch(url(`/inspections/3`), { headers: { 'X-Api-Key': 'yqeY9Rfl3jcXOfbtytLj3MqT1' } })
+	let raw = await fetch(`/article-short.json`, { headers: { 'X-Api-Key': 'yqeY9Rfl3jcXOfbtytLj3MqT1' } })
 	return await raw.json()
 }
 
