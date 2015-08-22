6e94a831e72ddd4a4f1d329a2f82d49779af7a75
Alasdair Smith <ali@alasdairsmith.co.uk>
2015-08-22 22:18:30 +0100

    Processing of text nodes

--- src/support/Processor.js
+++ src/support/Processor.js
@@ -6,7 +6,8 @@ export default class Processor {
 
 	process() {
 		return Object.assign({},
-			this.processHeader()
+			this.processHeader(),
+			this.processText()
 		)
 	}
 
@@ -27,5 +28,39 @@ export default class Processor {
 		}
 	}
 
+	processText() {
+		let nodes = this.data.nodes
+
+		let text = nodes.content.nodes
+			.filter((nodeId) => nodeId !== 'cover') // Filter out cover node - handled by processHeader()
+			.map((nodeId) => nodes[nodeId]) // Pull text nodes out using content node ids array
+			.map((node) => {
+				let content
+
+				if (node.type === 'paragraph') {
+					let textId = node.children[0]
+					let textNode = nodes[textId]
+
+					if ( ! textNode) throw new Error('Text node not found')
+
+					content = textNode.content
+				}
+				else if (node.type === 'heading') {
+					if ( ! node.content) throw new Error('Header content not found')
+
+					content = node.content
+				}
+				else {
+					throw new Error('Unknown node type')
+				}
+
+				return {
+					type: node.type,
+					content: content
+				}
+			});
+
+		return { text }
+	}
 }
 
