a42b35cc819ef92ac6f0d0cc41cd57e8ae66d654
Alasdair Smith <ali@alasdairsmith.co.uk>
2015-08-22 22:18:12 +0100

    Readd Processor class so git picks up case change on filename

---
+++ src/support/Processor.js
@@ -0,0 +1,31 @@
+export default class Processor {
+
+	constructor(data) {
+		this.data = data;
+	}
+
+	process() {
+		return Object.assign({},
+			this.processHeader()
+		)
+	}
+
+	processHeader() {
+		let nodes = this.data.nodes
+
+		let documentMeta = nodes.document
+		let publicationInfo = nodes.publication_info
+
+		// Pull author nodes out using author ids array
+		let authors = documentMeta.authors.map((id) =>  nodes[id])
+
+		return {
+			publishDate: new Date(publicationInfo.published_on),
+			title: documentMeta.title,
+			authors: authors,
+			doi: publicationInfo.doi
+		}
+	}
+
+}
+
