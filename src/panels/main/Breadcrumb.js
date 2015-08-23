4bbd7db153cdb0b47a640a3a9b7176651bde8bef
Alasdair Smith <ali@alasdairsmith.co.uk>
2015-08-23 12:21:16 +0100

    Breadcrumb renders text or image

--- src/panels/main/Breadcrumb.js
+++ src/panels/main/Breadcrumb.js
@@ -3,8 +3,12 @@ import React, { Component, PropTypes } from 'react'
 export default class Breadcrumb extends Component {
 
 	render() {
+		let {name, url, image} = this.props
+
 		return (
-			<a href={this.props.url}>{this.props.name}</a>
+			<a href={url}>
+				{ image ? <img src={image} title={name} /> : {name} }
+			</a>
 		)
 	}
 
